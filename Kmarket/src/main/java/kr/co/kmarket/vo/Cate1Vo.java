f95f94156165790804173465389801f2e33c6915
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-07-21 12:54:13 +0900

    2021/07/21

---
+++ Kmarket/src/main/java/kr/co/kmarket/vo/Cate1Vo.java
@@ -0,0 +1,9 @@
+package kr.co.kmarket.vo;
+
+import lombok.Data;
+
+@Data
+public class Cate1Vo {
+	private int code1;
+	private String title; 
+}
---
+++ Kmarket/src/main/java/kr/co/kmarket/vo/Cate2Vo.java
@@ -0,0 +1,12 @@
+package kr.co.kmarket.vo;
+
+import lombok.Data;
+
+@Data
+public class Cate2Vo {
+
+	private int seq;
+	private int code1;
+	private int code2;
+	private String title;
+}
